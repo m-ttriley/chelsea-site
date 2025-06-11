3d643c02c908d2d8927aec5a9a28b71811d94e0d
matt riley <matthew.vincent.riley@gmail.com>
2025-06-11 15:07:26 +0100

    fixes title width, fixes large height centering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,14 @@ import styles from '@/styles/Main.module.css';
 import HeroScroller from '../components/HeroScroller';
 
 export default function Page() {
-  return <>
-      <HeroScroller />
-      <div className={styles.footer}>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.</p>
+  return (
+    <div className={styles.main}>
+      <div>
+        <HeroScroller />
+        <div className={styles.footer}>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.</p>
+        </div>
       </div>
-    </>
+    </div>
+  );
 }
