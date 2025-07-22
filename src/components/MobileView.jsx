67e1e3a0c8f5b2a8624172d18d099574764f86e8
matt riley <matthew.vincent.riley@gmail.com>
2025-07-22 12:03:53 -0400

    Mobile styling tweaks

--- src/components/MobileView.jsx
+++ src/components/MobileView.jsx
@@ -178,7 +178,7 @@ const MobileView = () => {
                           <div className="ff-68716212bedecce39b8014a9__fields" data-ff-el="fields" style={{
                             width: '50%',
                             display: 'inline-block',
-                            paddingTop: '4px',
+                            paddingTop: '7px',
                           }}>
                             <div className="ff-68716212bedecce39b8014a9__field fd-form-group">
                               {!submitPending && <input
